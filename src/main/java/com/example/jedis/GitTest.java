fdac204cf729f1cd5d7cd53db68647ccdad654e1
yuexin <1614286627@qq.com>
2022-05-03 16:29:14 +0800

    firstcommit

--- src/main/java/com/example/jedis/GitTest.java
+++ src/main/java/com/example/jedis/GitTest.java
@@ -3,5 +3,7 @@ package com.example.jedis;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git ");
+        System.out.println("hello git2 ");
+
     }
 }
