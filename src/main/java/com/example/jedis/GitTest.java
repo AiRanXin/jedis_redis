787ede8c1f1732a8f226638afd5b6009069dbb8a
yuexin <1614286627@qq.com>
2022-05-03 17:53:00 +0800

    commit6

--- src/main/java/com/example/jedis/GitTest.java
+++ src/main/java/com/example/jedis/GitTest.java
@@ -9,6 +9,7 @@ public class GitTest {
         System.out.println("hello git4 ");
         System.out.println("hello git  hot-fix2 ");
         System.out.println("hello git push ");
+        System.out.println("hello git push1 ");
 
 
 
